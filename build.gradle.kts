ba45981f9634e7f1fe08566af2dfcbf3f7d76cf9
xue2sheng <andres.sevillano.vallejo@gmail.com>
2023-08-19 00:34:06 +0200

    ignore build

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.compose") version "1.4.3"
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "com.xue2sheng.kotlin.desktop"
+version = "1.0"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "test03"
+            packageVersion = "1.0.0"
+        }
+    }
+}
